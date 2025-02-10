e2e685de8e50d94f4348880cc9ab8ad6e193e30e
fahimfiroz42 <fahimfiroz.bd42@gmail.com>
2025-02-10 11:54:01 +0600

    type annotaion

---
+++ src/tutorial.ts
@@ -0,0 +1,12 @@
+let greeting:string="hello"
+
+greeting=greeting.toLocaleUpperCase()
+
+let age:number=20
+
+let isOnline:boolean=true
+
+// age='dfd'
+// isOnline=20
+// greeting=true
+// it is wrong type but my application is not crush becausd of type checking
